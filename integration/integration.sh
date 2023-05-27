3c719ef4e941993e7f3916eac22b677457a47026
yuria <yuria.soft0615@gmail.com>
2023-05-27 22:02:34 +0900

    インテグレーションテストの作成 (#5)
    * make exec_integration.sh
    
    * make github actions
    
    * make github actions
    
    * set UrusaiX mode
    
    * set UrusaiX mode
    
    * test
    
    * MacOSさん大文字と小文字は違いますよ！！！

---
+++ integration/integration.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+backendUsersStatus="$(curl backend:3000/users -o /dev/null -w '%{http_code}\n' -s)"
+if [[ "${backendUsersStatus}" -ne 200 ]]; then
+  echo "Backend users service is not ready yet. Status code: ${backendUsersStatus}"
+  exit 1
+fi
