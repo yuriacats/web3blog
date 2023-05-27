7d1fdfb547986ac727199127a55ba36fb41ed694
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-05-27 12:43:58 +0000

    set UrusaiX mode

--- Integration/integration.sh
+++ Integration/integration.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ux
 backendUsersStatus="$(curl backend:3000/users -o /dev/null -w '%{http_code}\n' -s)"
 if [[ "${backendUsersStatus}" -ne 200 ]]; then
   echo "Backend users service is not ready yet. Status code: ${backendUsersStatus}"
