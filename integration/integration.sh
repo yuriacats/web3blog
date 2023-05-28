74b1260e6b78790b5e59b73d49276f31be4ebaee
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-05-28 06:31:28 +0000

    make database

--- integration/integration.sh
+++ integration/integration.sh
@@ -4,3 +4,4 @@ if [[ "${backendUsersStatus}" -ne 200 ]]; then
   echo "Backend users service is not ready yet. Status code: ${backendUsersStatus}"
   exit 1
 fi
+mysql -h db -P 3306 -u root -ptoor -e "select * from author " webblog
