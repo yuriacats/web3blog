83cefbb13d24b72cb8ee7a14f94c1e77d39be025
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-05-28 08:55:43 +0000

    dbに接続されていることを確認した

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,22 @@
 import express from 'express'
+import * as mysql from 'promise-mysql'
 const app: express.Express = express()
 const port = process.env.PORT || 8000;
+const sql_user = process.env.SQL_USER;
+const sql_password = process.env.SQL_PASSWORD;
+const sql_host = process.env.SQL_HOST;
+// dev-containerで接続する際に、docker-composeのdbを使うのでそれ用の値をデフォルト値にしています。
+
+async function connection() {
+    const connection = await mysql.createConnection({
+        host: sql_host,
+        user: sql_user,
+        password: sql_password,
+        database: 'webblog',
+        multipleStatements: true
+    });
+    return connection
+}
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -16,18 +32,12 @@ app.listen(port, () => {
     console.log(`start on port ${port}`)
 })
 
-type User = {
-    id: number,
-    name: string,
-    email: string,
-}
-
-const users: User[] = [
-    { id: 1, name: "John", email: "user1@test.local" },
-    { id: 2, name: "John.J", email: "user2@test.local" },
-    { id: 3, name: "John.M", email: "user3@test.local" },
-]
 
 app.get('/users', (req: express.Request, res: express.Response) => {
-    res.json(users)
+    connection().then(connection => {
+        const result = connection.query('SELECT * FROM author')
+        connection.end();
+        console.log(result)
+        return result
+    }).then((rows) => res.json(rows));
 });
--- exec_integration.sh
+++ exec_integration.sh
@@ -2,7 +2,7 @@
 set -ux
 docker-compose build
 docker-compose up -d 
-docker-compose run integration ./integration.sh
+docker-compose exec integration ./integration.sh
 exit_code=$?
 docker-compose down
 if [ ${exit_code} -ne 0 ]; then
