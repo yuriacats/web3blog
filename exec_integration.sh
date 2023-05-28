c71681ace68a2cb2a13d3505c4a765fb6de27a3b
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-05-28 09:27:19 +0000

    docker-compose execがターミナルを割り当てるので、割り当てを無効化するコマンドの追加

--- exec_integration.sh
+++ exec_integration.sh
@@ -2,7 +2,7 @@
 set -ux
 docker-compose build
 docker-compose up -d 
-docker-compose exec integration ./integration.sh
+docker-compose exec -T integration ./integration.sh
 exit_code=$?
 docker-compose down
 if [ ${exit_code} -ne 0 ]; then
