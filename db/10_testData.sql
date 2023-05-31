56eadcd513a8d45e292df6756aaf1aa34f0c360d
yuria <yuria.soft0615@gmail.com>
2023-06-01 00:27:33 +0900

    make  a api url (#11)

--- db/10_testData.sql
+++ db/10_testData.sql
@@ -1,4 +1,6 @@
 USE webblog;
+SET CHARACTER_SET_CLIENT = utf8;
+SET CHARACTER_SET_CONNECTION = utf8;
 INSERT INTO tag_name(name) VALUES('React'),('Vue'),('Elm');
 INSERT INTO post(slug) VALUES('8557AF75A904DEEF4E00'),('DBAE5FBC068F850F4CE5'),('DE9E321CA9C94A18BC0C');
 INSERT INTO tag_post VALUES(1,3),(1,1),(2,1),(3,2);
@@ -6,6 +8,7 @@ INSERT INTO author(name) VALUES('yuria');
 INSERT INTO author_secret VALUES(1,'yuria@sample.local','hashd_password');
 INSERT INTO post_revision(title,author_id,post_id,public,post_data) VALUES
 ('テストデータ',1,1,1,"テストテストテストテスト"),
-('テストデータ2',1,2,1,"テスト2テスト2テスト2テスト2"),
+('テストデータ2',1,2,1,"テスト2テスト2テスト2テスト2");
+INSERT INTO post_revision(title,author_id,post_id,public,post_data) VALUES
 ('テストデータ3',1,1,1,"~~テスト~~ **テスト** \n- テスト\n\n# テスト");
 
--- integration/integration.sh
+++ integration/integration.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 set -ux
+
+URL_STATUS_TEST(){
+  local url="$1"
+  local backendUsersStatus="$(curl "${url}" -o /dev/null -w '%{http_code}\n' -s)"
+if [[ "${backendUsersStatus}" -ne 200 ]]; then
+  echo "${url} is not ready yet. Status code: ${backendUsersStatus}"
+  exit 1
+fi
+}
+
+URL_GREP_TEST(){
+local url="$1"
+local target="$2"
+URL_STATUS_TEST ${url}
+local frontendTest="$(curl "${url}" | grep "${target}")"
+if [ $? -ne 0 ]; then
+  echo "${url} :page does not have ${target} "
+  exit 1
+fi
+}
+
 count=0
 until mysqladmin ping -ptoor -h db ; do
   count=$((count+1))
@@ -12,14 +33,8 @@ until mysqladmin ping -ptoor -h db ; do
 done
 mysql -h db -P 3306 -u backend -ptoor -e "select * from author " webblog
 
-backendUsersStatus="$(curl backend:8000/users -o /dev/null -w '%{http_code}\n' -s)"
-if [[ "${backendUsersStatus}" -ne 200 ]]; then
-  echo "Backend users service is not ready yet. Status code: ${backendUsersStatus}"
-  exit 1
-fi
 
-frontendTest="$(curl frontend:3000 | grep yuria)"
-if [ $? -ne 0 ]; then
-  echo "frontend error"
-  exit 1
-fi
+URL_GREP_TEST "backend:8000/users" "yuria" 
+URL_GREP_TEST "backend:8000/posts/8557AF75A904DEEF4E00" "テスト"
+URL_STATUS_TEST "backend:8000/posts/855F4E00"
+URL_GREP_TEST "frontend:3000" "yuria"
