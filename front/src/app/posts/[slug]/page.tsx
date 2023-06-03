e60f468ea42a7f49e85b679d0a24f577e20e11e9
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-06-03 07:42:23 +0000

    fix how to validate slug

--- front/src/app/posts/[slug]/page.tsx
+++ front/src/app/posts/[slug]/page.tsx
@@ -1,10 +1,8 @@
 import { notFound } from 'next/navigation';
 import { z } from "zod";
 import React, { Suspense } from "react";
-import { postData } from "lib/featch_author";
-const slugName = z.object({
-    slug: z.string().length(20)
-});
+import { postData } from "lib/fetch_author";
+const slugName = z.string().length(20)
 const PageContents = async ({ slug }: { slug: string }): Promise<React.ReactElement> => {
     console.log(`PageContents slug ${slug}`)
     const postdata = await postData(slug);
@@ -25,7 +23,7 @@ const PageContents = async ({ slug }: { slug: string }): Promise<React.ReactElem
 export default function Home({ params }: { params: { slug: string } }): React.ReactNode {
 
     try {
-        const page_name = (slugName.parse(params)).slug;
+        const page_name = slugName.parse(params.slug);
 
         return (
             <main >
