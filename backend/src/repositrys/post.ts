14662403bb24dd5fe01df94f21166e5efb874dca
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-06-04 11:58:28 +0000

    一旦Push

--- backend/src/repositrys/post.ts
+++ backend/src/repositrys/post.ts
@@ -3,29 +3,32 @@ import { Post, Slug } from "../interface";
 import { z } from "zod";
 import { get_author } from "./author";
 
-const sluggedRevisionSchema = z.object({
+const suluggedRevisionSchema = z.object({
   title: z.string(),
   author_id: z.number(),
   create_date: z.date(),
   post_data: z.string(),
   slug: z.string().length(20),
 });
-const sluggedRevisionsSchema = z.array(sluggedRevisionSchema).min(1);
+const sluggedRevisionsSchema = z.array(suluggedRevisionSchema).min(1);
 
 export const getPost = async (slug: Slug): Promise<Post> => {
   const conn = await connection();
-  const postsQuery = sluggedRevisionsSchema
-    .parse(
-      await conn.query(
-        ` SELECT title, author_id, post_revision.create_date, post_data,slug 
+  const postsQuery = await conn.query(
+    ` SELECT title, author_id, post_revision.create_date, post_data,slug 
         FROM post_revision JOIN post ON post_revision.post_id=post.id 
         WHERE slug= ? ;
          `,
-        [slug]
-      )
-    )
+    [slug]
+  );
+  const sluggedRevisions = sluggedRevisionsSchema
+    .parse(postsQuery)
+    // これsortとして成り立ってる？
     .sort((l, r) => (l.create_date > r.create_date ? 1 : -1));
-  const targetPost = sluggedRevisionSchema.parse(postsQuery[0]);
+  const targetPost = sluggedRevisions[0];
+  if (targetPost === undefined) {
+    throw Error;
+  }
   const authorName = (await get_author(targetPost.author_id)).name;
   console.log(targetPost.create_date);
 
