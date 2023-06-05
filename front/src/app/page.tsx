4af35460257eaa751ffac95ae6fa8e9fbd94ddf8
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-06-05 09:19:36 +0000

    仮トップページの作成

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home(): React.ReactNode {
       <h1> yuria Top Page</h1>
       <p>blog記事（見本）</p>
       <Link href="posts/8557AF75A904DEEF4E00">
-        <a>Rustで一日でDiscordBotを作る(一部転載)</a>
+        <a>Rustで一日でDiscordBotを作る(自分の記事から一部転載)</a>
       </Link>
     </main>
   );
