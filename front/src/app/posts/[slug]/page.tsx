094d56ad04dccd38c4281aad5e49527799cdd084
HimekaTsuchida <yuria.soft0615@gmail.com>
2023-06-29 13:33:46 +0000

    page.tsxのコンポーネントを分割した

--- front/src/app/posts/[slug]/page.tsx
+++ front/src/app/posts/[slug]/page.tsx
@@ -1,33 +1,25 @@
 import { notFound } from "next/navigation";
 import { z } from "zod";
-import { unified } from "unified";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
-import rehypeReact from "rehype-react";
-import { createElement } from "react";
 import React, { Suspense } from "react";
 import { fetchPost } from "lib/fetchPost";
+import { MdToHtml } from "component/mdToHtml";
+import { PageHead } from "component/pageHead";
 const slugSchema = z.string().length(20);
 const PageContents = async ({
   slug,
 }: {
   slug: string;
 }): Promise<React.ReactElement> => {
-  console.log(`PageContents slug ${slug}`);
   const post = await fetchPost(slug);
-  const HTMLcontent = await unified()
-    .use(remarkParse)
-    .use(remarkRehype)
-    .use(rehypeReact, { createElement })
-    .process(post.content);
 
   return (
     <>
-      <h1>{post.title}</h1>
-      <p>
-        {post.author}: {post.updateDate.toLocaleString()}
-      </p>
-      <div>{HTMLcontent.result}</div>
+      <PageHead
+        title={post.title}
+        author={post.author}
+        date={post.updateDate}
+      />
+      <MdToHtml md={post.content} />
     </>
   );
 };
@@ -45,7 +37,6 @@ export default function Home({
   return (
     <main>
       <Suspense fallback={<></>}>
-        {/* @ts-expect-error Async Server Component */}
         <PageContents slug={slugParseResult.data} />
       </Suspense>
     </main>
